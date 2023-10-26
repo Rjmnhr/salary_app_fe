8f3f2010b8a7c5606e01aa41f9edcd4ac4b24757
Rjmnhr <88718714+Rjmnhr@users.noreply.github.com>
2023-10-26 12:06:41 +0530

    Update index.js

--- src/pages/executive-reports/index.js
+++ src/pages/executive-reports/index.js
@@ -50,7 +50,7 @@ const ExecutiveReports = () => {
                     ₹ 25,000
                   </p>
                   <p>Receive Instantly</p>
-                  <CheckoutComponent price={"price_1O2oj5SHdxsAYvlqZD5LRK6Q"} />
+                  <CheckoutComponent price={"price_1O5LKYDNZni9rE7FT1E5VSrW"} />
                 </div>
               </Card>
             </Badge.Ribbon>
