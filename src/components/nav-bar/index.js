ee4632685fb8302b89b3a34a39dfac558e74c064
rjmnhr1 <renjith.cm@refactor.academy>
2023-11-06 12:23:41 +0530

    update

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -140,7 +140,7 @@ const NavBar = ({ scrollToContact }) => {
                   className={activeLink === "executive-reports" ? "active" : ""}
                 >
                   <a href="/executive-reports">
-                    Executive Compensation Reports
+                    Executive Compensation Benchmarking
                   </a>
                 </li>
 
