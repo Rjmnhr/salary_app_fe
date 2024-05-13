efda9b97a31d797eef32a10346454a4c4ea46303
rjmnhr1 <renjith.cm@refactor.academy>
2024-05-13 14:49:51 +0530

    updates

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  // baseURL: "https://backend.equipaypartners.com", // Replace with your API base URL
-  baseURL: "http://localhost:8003",
+  baseURL: "https://backend.equipaypartners.com",
+  // baseURL: "http://localhost:8003",
 });
 
 export default AxiosInstance;
